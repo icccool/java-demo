39b0ffd484435ae24c94faed7f8f3950a4888843
icccool <wlish.ml@gmail.com>
2018-05-13 16:55:04 +0800

    1.8

--- src/main/java/com/jdk18/LambdaTest.java
+++ src/main/java/com/jdk18/LambdaTest.java
@@ -1,11 +1,36 @@
 package com.jdk18;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by john on 18/1/5.
  */
 public class LambdaTest {
 
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
+    	//=======================================================================================
+    	// Java 8之前：
+    	new Thread(new Runnable() {
+    	    @Override
+    	    public void run() {
+    	    System.out.println("Before Java8, too much code for too little to do");
+    	    }
+    	}).start();
+    	//Java 8方式：
+		new Thread(() -> System.out.println("In Java8, Lambda expression rocks !!")).start();
+    	//=======================================================================================
+    	// Java 8之前：
+    	List<String> features = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+    	for (String feature : features) {
+    	    System.out.println(feature);
+    	}
+    	//Java 8方式：
+		List features2 = Arrays.asList("Lambdas", "Default Method", "Stream API", "Date and Time API");
+		features2.forEach(n -> System.out.println(n));
+    	//=======================================================================================
+    	
+    	
         MyString reverse = (str)-> {
 
             return "";
