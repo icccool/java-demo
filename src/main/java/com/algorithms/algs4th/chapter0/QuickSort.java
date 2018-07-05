5ad304ef5dcb0148a74e261aae394903393caff9
wangjibo <wlish.ml@gmail.com>
2018-07-05 18:37:39 +0800

    algorithms

--- src/main/java/com/algorithms/algs4th/chapter0/QuickSort.java
+++ src/main/java/com/algorithms/algs4th/chapter0/QuickSort.java
@@ -1,6 +1,6 @@
 package com.algorithms.algs4th.chapter0;
 
-import com.algorithms.sort.SortUtil;
+import com.algorithms.SortUtils;
 
 /**
  * Created by WANG on 2018/7/5.
@@ -9,30 +9,48 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-        int[] arr = {2, 1, 3};
+        int[] arr = {6, 9, 1, 8, 15, 0, 8};
+        partition(arr, 0, arr.length - 1);
+        SortUtils.print(arr);
 
-        int p = arr.length / 2;
-        partition(arr, 0, p, arr.length - 1);
+//        int p = arr.length / 2;
+//        partition(arr, 0, p, arr.length - 1);
+//        SortUtils.print(arr);
     }
 
-    public static int partition(int[] arr,int lo,int p,int hi) {
-        int key = arr[lo];
-        int i = lo, j = hi;
-        while(true){
-            while (i < p && arr[i] > arr[key]){
-                i++;
-                break;
+    public static void partition(int[] arr, int lo, int hi) {
+        int i = lo, j = hi + 1;
+        int key = lo;
+        while (true){
+            while (arr[++i] > arr[key]) {
+                if (i >= hi)
+                    break;
             }
-            while (j > p && arr[j] < arr[key]){
-                j--;
-                break;
+            while (arr[--j] < arr[key]) {
+                if (j <= lo)
+                    break;
             }
             if(i >= j){
                 break;
             }
-            SortUtil.exch(arr, i, j);
+            SortUtils.exch(arr, i, j);
         }
-        SortUtil.exch(arr, key, i);
-        return i;
+        SortUtils.exch(arr, j, key);
     }
+
+    public static void sort(int[] arr, int lo, int hi) {
+        if (lo < hi) {
+            int key = lo;
+            int p = lo;
+            for (int i = lo + 1; i <= hi; i++) {
+                if (arr[i] < arr[key]) {
+                    SortUtils.exch(arr, ++p, i);
+                }
+            }
+            SortUtils.exch(arr, p, key);
+            sort(arr, lo, p - 1);
+            sort(arr, p + 1, hi);
+        }
+    }
+
 }
