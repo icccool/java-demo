fc82ae0ee622a736f634d3ff1af7b43f6b2bc3c4
icccool <wlish.ml@gmail.com>
2018-07-21 22:04:29 +0800

    algs

--- src/main/java/com/algorithms/algs4th/chapter1/MinPQTest.java
+++ src/main/java/com/algorithms/algs4th/chapter1/MinPQTest.java
@@ -1,18 +1,19 @@
 package com.algorithms.algs4th.chapter1;
 
 public class MinPQTest {
-	
-	private static int M = 5;
-	
+
+	private static int M = 3;
+
 	public static void main(String[] args) {
-		Character[] c = { '3', '5', '6', '3', '2', '9' };
-		MinPQ<Character> pq = new MinPQ<Character>(M);
+		Character[] c = { '0', '5', '6', '5', '2', '9','1' };
+		MinPQ<Character> pq = new MinPQ<Character>(M + 1);
 		for (int i = 0; i < c.length; i++) {
 			pq.insert(c[i]);
-			if(pq.szie() > M) {
+			if (pq.szie() > M) {
+				pq.delMin();
 			}
 		}
 		pq.print();
 	}
-	
+
 }
