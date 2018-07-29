ca5896bddb893039f097a793d7f3dd38719e3e74
icccool <wlish.ml@gmail.com>
2018-07-29 21:26:28 +0800

    algs

--- src/main/java/com/algorithms/sort/BSTTest.java
+++ src/main/java/com/algorithms/sort/BSTTest.java
@@ -2,20 +2,23 @@ package com.algorithms.sort;
 
 public class BSTTest {
 	public static void main(String[] args) {
-		BST<String,String> bst = new BST<String,String>();
+		BST<String, String> bst = new BST<String, String>();
 		bst.put("S", "SSS");
 		bst.put("E", "EEE");
 		bst.put("X", "xxx");
 		bst.put("A", "AAA");
 		bst.put("R", "RRR");
-		//get
+		// get
 		System.out.println("------------get------------");
 		String str = bst.get("X");
 		System.out.println(str);
-		//size
+		// size
 		System.out.println("------------size------------");
 		System.out.println(bst.size());
-		//print
+		// delete
+		System.out.println("------------print------------");
+		bst.print();
+		// print
 		System.out.println("------------print------------");
 		bst.print();
 	}
