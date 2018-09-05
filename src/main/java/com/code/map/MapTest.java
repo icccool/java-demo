f0259b39892d52ad59d7dc494b5ebcc976508b0b
wangjibo <wlish.ml@gmail.com>
2018-09-05 10:06:30 +0800

    移除map中元素

--- src/main/java/com/code/map/MapTest.java
+++ src/main/java/com/code/map/MapTest.java
@@ -1,10 +1,32 @@
 package com.code.map;
 
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class MapTest {
 	public static void main(String[] args) {
-		Map<String,String> ls = new HashMap<String, String>();
+		Map<String,String> map = new HashMap<String, String>();
+		map.put("1","a");
+		map.put("2","b");
+		map.put("3","c");
+		map.put("4","d");
+//		for (String key : map.keySet()){
+//			if(key.equals("1")){
+//				map.remove(key);
+//			}
+//		}
+//		System.out.println(map);
+
+
+		Iterator<String> itr = map.keySet().iterator();
+		while (itr.hasNext()){
+			String key = itr.next();
+			if(key.equals("1")){
+				itr.remove();
+			}
+		}
+		System.out.println(map);
+
 	}
 }
