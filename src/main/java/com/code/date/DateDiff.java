9bd9c24b1d336e9b200f8375fc08820ae0d298be
WangJibo <2880985654@qq.com>
2020-02-20 16:45:13 +0800

    feat(init): init

--- src/main/java/com/code/date/DateDiff.java
+++ src/main/java/com/code/date/DateDiff.java
@@ -7,24 +7,24 @@ import java.util.Date;
  */
 public class DateDiff {
 
-
+    static long nd = 1000 * 24 * 60 * 60;
+    static long nh = 1000 * 60 * 60;
+    static long nm = 1000 * 60;
 
     public static void main(String[] args) {
-
-        DateUtil.parse("2018-8-29 16:33:20");
-        System.out.println(getDatePoor(DateUtil.parse("2018-08-29 16:35:20"),new Date()));
-
-        long diff = DateUtil.parse("2018-08-29 16:38:00").getTime() - new Date().getTime();
+        Date compareDate = DateUtil.parse("2019-11-05 16:38:00");
+        Date currentDate = new Date();
+        long diff = currentDate.getTime() - compareDate.getTime();
         System.out.println("相差" + (diff / 1000) + "秒");
-        System.out.println(new Date(1535533104032L));
-
+        System.out.println("相差" + (diff / nd) + "天");
+        if ((diff / nd) > -1) {
+            System.out.println("未到有效期");
+        } else {
+            System.out.println("到有效期");
+        }
     }
 
     public static String getDatePoor(Date endDate, Date nowDate) {
-
-        long nd = 1000 * 24 * 60 * 60;
-        long nh = 1000 * 60 * 60;
-        long nm = 1000 * 60;
         // long ns = 1000;
         // 获得两个时间的毫秒时间差异
         long diff = endDate.getTime() - nowDate.getTime();
---
+++ src/main/java/com/mq/Test.java
@@ -0,0 +1,56 @@
+package com.mq;
+
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.client.producer.DefaultMQProducer;
+import org.apache.rocketmq.client.producer.SendResult;
+import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
+
+/**
+ * @Author WANG JI BO
+ * @Date 2019/11/11 8:59 PM
+ * @Description
+ **/
+public class Test {
+
+    public static void main(String[] args) {
+
+        try {
+            // 声明并初始化一个producer
+            // 需要一个producer group名字作为构造方法的参数，这里为producer1
+            DefaultMQProducer producer = new DefaultMQProducer("addmantenance");
+            producer.setVipChannelEnabled(false);
+            // 设置NameServer地址,此处应改为实际NameServer地址，多个地址之间用；分隔
+            // NameServer的地址必须有
+            // producer.setClientIP("119.23.211.22");
+            // producer.setInstanceName("Producer");
+            producer.setNamesrvAddr("192.168.10.66:9876");
+
+            // 调用start()方法启动一个producer实例
+            producer.start();
+
+            // 发送1条消息到Topic为TopicTest，tag为TagA，消息内容为“Hello RocketMQ”拼接上i的值
+            try {
+                // 封装消息
+                Message msg = new Message("DistributionTopic",// topic
+                        "storereturn",// tag
+                        ("Hello RocketMQ").getBytes(RemotingHelper.DEFAULT_CHARSET)// body
+                );
+                // 调用producer的send()方法发送消息
+                // 这里调用的是同步的方式，所以会有返回结果
+                SendResult sendResult = producer.send(msg);
+                // 打印返回结果
+                System.out.println(sendResult);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            //发送完消息之后，调用shutdown()方法关闭producer
+            System.out.println("send success");
+            producer.shutdown();
+        } catch (MQClientException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
